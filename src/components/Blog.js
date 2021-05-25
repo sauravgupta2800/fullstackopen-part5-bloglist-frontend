ccac756047db0d33c63d57eb6cea0fb5ab3cfed6
sauravgupta2800 <sauravgupta2800@gmail.com>
2021-05-25 09:15:43 +0530

    5.7* Blog list frontend, step7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,44 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import React from "react";
+import Togglable from "../components/Togglable";
+
+const Blog = ({ blog: { title, author, url, likes } }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return (
+    <div style={blogStyle}>
+      {title} by {author}
+      <Togglable
+        showFormBtnText={"Show Details"}
+        hideFormBtnText={"Hide Details"}
+      >
+        <div>
+          <div>
+            <span>Title - </span>
+            <span>{title}</span>
+          </div>
+          <div>
+            <span>Author - </span>
+            <span>{author}</span>
+          </div>
+          <div>
+            <span>Url - </span>
+            <span>{url}</span>
+          </div>
+          <div>
+            <span>Likes - </span>
+            <span>{likes}</span>
+            <button>like</button>
+          </div>
+        </div>
+      </Togglable>
+    </div>
+  );
+};
 
 export default Blog;
